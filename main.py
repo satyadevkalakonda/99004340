ff91ebe06e875d1df8c3a9bc403818a77ccc95a3
99004340 <satyadev.k@llts.com>
2021-06-03 10:24:36 +0530

    Added output data funcionality

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-'''
+"""
 Advanced Python Project
-'''
+"""
+
 import openpyxl
+from openpyxl import Workbook
+
 
 class CheckPs:
-    def __init__(self,n_1):
-        self.n=n_1
+    def __init__(self, n_1):
+        self.n = n_1
+
     def check_ps(self):
         path = "data.xlsx"
         wb_obj = openpyxl.load_workbook(path)
@@ -14,9 +18,10 @@ class CheckPs:
         c = 0
         for i in range(1, max_r + 1):
             cell_obj = sheet_obj.cell(row=i, column=1)
-            if(cell_obj.value == self.n):
+            if cell_obj.value == self.n:
                 c = 1
         return c
+
 
 class PrintPs:
     @staticmethod
@@ -29,46 +34,54 @@ class PrintPs:
             cell_obj = sheet_obj.cell(row=i, column=1)
             print(cell_obj.value)
 
+
 class ExactPs:
-    def __init__(self,n_1):
-        self.n=n_1
+    def __init__(self, n_1):
+        self.n = n_1
+
     def exactps(self):
         path = "data.xlsx"
         wb_obj = openpyxl.load_workbook(path)
         sheet_obj = wb_obj["Sheet2"]
         max_r = sheet_obj.max_row
-        max_c=sheet_obj.max_column
-        c = 0
         for i in range(1, max_r + 1):
             cell_obj = sheet_obj.cell(row=i, column=1)
-            if (cell_obj.value == self.n):
+            if cell_obj.value == self.n:
                 return i
+
+
 class Detailsps:
-    def __init__(self,n):
-        self.n=n
+    def __init__(self, n):
+        self.n = n
+
     def detailsPs(self):
         path = "data.xlsx"
         wb_obj = openpyxl.load_workbook(path)
         sheet_obj_1 = wb_obj["Sheet1"]
         max_c = sheet_obj_1.max_column
-        i=2
+        # out put file creation
+        wb = Workbook()
+        filepath = "output.xlsx"
+        wb.save(filepath)
+        wb = openpyxl.load_workbook(filepath)
+        sheet = wb.active
         for j in range(2, max_c + 1):
-            cell_obj_1=sheet_obj_1.cell(row=1,column=i)
-            i+=1
+            cell_obj_1 = sheet_obj_1.cell(row=1, column=j)
+            sheet.cell(row=j, column=1).value = cell_obj_1.value
             cell_obj_2 = sheet_obj_1.cell(row=self.n, column=j)
-            str="The {c1} Grade is {c2}".format(c1=cell_obj_1.value,c2=cell_obj_2.value)
+            sheet.cell(row=j, column=2).value = cell_obj_2.value
+            str = "The {c1} Grade is {c2}".format(c1=cell_obj_1.value, c2=cell_obj_2.value)
             print(str)
-
         sheet_obj_2 = wb_obj["Sheet2"]
         max_c = sheet_obj_2.max_column
-        i = 2
         for j in range(2, max_c + 1):
-            cell_obj_1 = sheet_obj_2.cell(row=1, column=i)
-            i += 1
+            cell_obj_1 = sheet_obj_2.cell(row=1, column=j)
+            sheet.cell(row=j, column=3).value = cell_obj_1.value
             cell_obj_2 = sheet_obj_2.cell(row=self.n, column=j)
+            sheet.cell(row=j, column=4).value = cell_obj_2.value
             str = "The {c1}  is {c2}".format(c1=cell_obj_1.value, c2=cell_obj_2.value)
             print(str)
-
+        wb.save(filepath)
 
 def main():
     print("The List of Ps ID :")
@@ -77,15 +90,15 @@ def main():
     n_1 = int(input())
     c_1 = CheckPs(n_1)
     val = c_1.check_ps()
-    if(val == 1):
+    if val == 1:
         print("Present")
-        E_1=ExactPs(n_1)
-        A=E_1.exactps()
-        D_1=Detailsps(A)
+        E_1 = ExactPs(n_1)
+        A = E_1.exactps()
+        D_1 = Detailsps(A)
         D_1.detailsPs()
     else:
         print("Not Present")
 
+
 if __name__ == "__main__":
     main()
-
